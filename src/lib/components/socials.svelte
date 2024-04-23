274a92158131a0e6cb823b1500d707174d826d2b
Graham Zemel <grahamzemel@Grahams-MacBook-Pro.local>
2024-04-22 23:57:13 -0400

    updated contact card

--- src/lib/components/socials.svelte
+++ src/lib/components/socials.svelte
@@ -6,28 +6,7 @@
 
 <div class="socials-row custom-fade-in-socials">
   <p class="font-light lg:block md:block hidden">Socials:</p>
-  <a
-    href="https://github.com/grahamzemel"
-    target="_blank"
-    rel="noopener noreferer"
-  >
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      role="img"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      stroke-width="1.8"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      class="icon"
-    >
-      <title>GitHub</title>
-      <path
-        d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
-      />
-    </svg>
-  </a>
+  
   <a
     href="https://www.linkedin.com/in/grahamzemel/"
     target="_blank"
@@ -93,6 +72,49 @@
       <path
         d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"
       />
+    </svg>
+  </a>
+  <a
+    href="https://github.com/grahamzemel"
+    target="_blank"
+    rel="noopener noreferer"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      role="img"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="1.8"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="icon"
+    >
+      <title>GitHub</title>
+      <path
+        d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
+      />
+    </svg>
+  </a>
+  <a
+    href="https://grahamzemel.com/pass"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      role="img"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="1.8"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="icon"
+    >
+      <title>Contact Card</title>
+      <path d="M12 2c-2.7 0-5.8 1.5-6 4.5-0.1 1.4 0.3 2.7 1 3.7 0.7 1 0.8 2.2 0.5 3.3-0.3 1.1-1.2 2.2-2.5 2.5-2 0.5-3.5 2.2-3.5 4.2v1.6c0 0.9 0.7 1.6 1.6 1.6h16.8c0.9 0 1.6-0.7 1.6-1.6v-1.6c0-2-1.5-3.7-3.5-4.2-1.3-0.3-2.2-1.4-2.5-2.5-0.3-1.1-0.2-2.3 0.5-3.3 0.7-1 1.1-2.3 1-3.7-0.2-3-3.3-4.5-6-4.5z"/>
+
     </svg>
   </a>
 </div>
