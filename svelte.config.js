c9eb6f40ee5dfec1a1ca608b0586436036fc1805
Graham Zemel <grahamzemel@gmail.com>
2026-02-07 18:27:14 -0700

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -14,10 +14,8 @@ const config = {
   ],
 
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(),
+    // Vercel adapter with explicit runtime to avoid build-time Node version mismatches.
+    adapter: adapter({ runtime: "nodejs18.x" }),
     csp: {
       directives: {
         "default-src": ["*", "unsafe-inline", "unsafe-eval"],
